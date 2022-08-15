4e61ffae5240c8713d14d9dd80abb8872ab38592
Tiago Santos do Nascimento <tiagosn06@gmail.com>
2022-08-15 10:38:09 -0300

    cria uma função para fazer o processamento de um arquivo txt criando um csv correspontente

--- main.py
+++ main.py
@@ -2,58 +2,17 @@ import spacy
 import pandas as pd
 from spacy.tokens.doc import Doc
 from pandas import DataFrame
-
-
-def convert_str_to_doc(text: str, lang: str) -> Doc:
-    lang_available = {
-        "pt": "pt_core_news_lg",
-        "en": "en_core_web_trf",
-        "es": "es_dep_news_trf",
-        "fr": "fr_dep_news_trf",
-        "de": "de_dep_news_trf",
-        "it": "it_core_news_lg",
-    }
-
-    pipeline = spacy.load(lang_available[lang])
-
-    return pipeline(text)
-
-
-def convert_doc_to_df(doc: Doc) -> DataFrame:
-    rows = []
-    columns = ["text", "lemma", "pos", "tag", "morph",
-               "ent_type", "is_punct", "dep", "sentiment"]
-    for token in doc:
-        rows.append([token.text, token.lemma_, token.pos_, token.tag_,
-                     token.morph, token.ent_type_, token.is_punct,
-                     token.dep_, token.sentiment])
-
-    return pd.DataFrame(rows, columns=columns)
-
-
-def load_raw_text(file_path) -> str:
-    text = ""
-    try:
-        with open(file_path, encoding='utf-8') as f:
-            text = text + f.read()
-    except OSError as err:
-        print(f'An OSError occurred when trying to open the file: {err}')
-    except Exception as exc:
-        print(f'An unpexted error occurred when trying to open the file: {exc}')
-
-    return text
+from utils.txt_to_csv import txt_to_csv
 
 
 def main():
-    text_name = 'historiasSemData'
+    text_name = 'reliquias'
     genero = 'conto'
-    file_path = f"datalake/machado_de_assis/{genero}/{text_name}.txt"
-    raw_data = load_raw_text(file_path)
+    dataset_name = 'machado_de_assis'
+    file_path = f"datalake/{dataset_name}/{genero}/{text_name}.txt"
     lang = "pt"
-    doc = convert_str_to_doc(raw_data, lang)
-    df = convert_doc_to_df(doc)
-    csv_file_name = f"{text_name}.csv"
-    df.to_csv(csv_file_name)
+    csv_file_name = f"data_warehouse/{dataset_name}/{genero}/{text_name}.csv"
+    txt_to_csv(file_path, csv_file_name, lang)
 
 
 if __name__ == '__main__':
---
+++ utils/txt_to_csv.py
@@ -0,0 +1,48 @@
+import spacy
+import pandas as pd
+from spacy.tokens.doc import Doc
+
+
+def convert_str_to_doc(text: str, lang: str) -> Doc:
+    lang_available = {
+        "pt": "pt_core_news_lg",
+        "en": "en_core_web_trf",
+        "es": "es_dep_news_trf",
+        "fr": "fr_dep_news_trf",
+        "de": "de_dep_news_trf",
+        "it": "it_core_news_lg",
+    }
+
+    pipeline = spacy.load(lang_available[lang])
+
+    return pipeline(text)
+
+
+def doc_to_df(doc: Doc) -> pd.DataFrame:
+    rows = []
+    columns = ["text", "lemma", "pos", "tag", "morph",
+               "ent_type", "is_punct", "dep", "sentiment"]
+    for token in doc:
+        rows.append([token.text, token.lemma_, token.pos_, token.tag_,
+                     token.morph, token.ent_type_, token.is_punct,
+                     token.dep_, token.sentiment])
+
+    return pd.DataFrame(rows, columns=columns)
+
+
+def load_raw_text(file_path) -> str:
+    text = ""
+    try:
+        with open(file_path, encoding='utf-8') as f:
+            text = text + f.read()
+    except OSError as err:
+        print(f'An OSError occurred when trying to open the file: {err}')
+    except Exception as exc:
+        print(f'An unpexted error occurred when trying to open the file: {exc}')
+
+    return text
+
+
+def txt_to_csv(input_file_path: str, output_path: str, lang: str):
+    df = doc_to_df(convert_str_to_doc(load_raw_text(input_file_path), lang))
+    return df.to_csv(output_path)
