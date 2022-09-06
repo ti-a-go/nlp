f94a33d54647fab884cfd8a81efb418403a30a6c
Tiago Santos do Nascimento <tiagosn06@gmail.com>
2022-09-06 20:32:31 -0300

    use the words list in memory instead of creating a file to save it

--- _wordcloud.py
+++ _wordcloud.py
@@ -2,18 +2,30 @@ import os
 from wordcloud import WordCloud
 import spacy
 
-file_path = 'datalake/machado_de_assis/romance/MemoriasBras.txt'
-with open(os.path.join('datalake/machado_de_assis/romance/MemoriasBras.txt'), 'r') as file_1:
-    text = file_1.read()
+
+# inputs:
+# txt_file, words_file, image_file
+txt_file = 'datalake/machado_de_assis/romance/MemoriasBras.txt'
+words_file = 'data_warehouse/wordcloud/machado_de_assis/memoriasBras.txt'
+image_file = 'data_warehouse/wordcloud/machado_de_assis/memoriasBras.png'
+
+
+with open(os.path.join(txt_file), 'r') as f:
+    text = f.read()
+# Carrega o modelo da língua portuguesa
 nlp = spacy.load('pt_core_news_lg')
+# Processa os dados e cria um Doc
 doc = nlp(text)
-with open('data_warehouse/wordcloud/machado_de_assis/memoriasBras.txt', 'w+') as file_2:
-    for token in doc:
-        if token.pos_ in ['PROPN']:
-            if token.text != 'SE':
-                file_2.write(f' {token.text} \n')
-with open('data_warehouse/wordcloud/machado_de_assis/memoriasBras.txt', 'r') as file:
-    wordcloud = WordCloud(width=2000, height=1000).generate(file.read())
+# cria a lista de palavras
+words = ''
+for token in doc:
+    if token.pos_ == 'PROPN':
+        if token.text != 'SE':
+            words += token.text_with_ws
+# cria a wordcloud
+wordcloud = WordCloud(width=2000, height=1000).generate(words)
+# salva a wordcloud no formato image
+with open(words_file, 'r') as f:
     image = wordcloud.to_image()
-    image.save(os.path.join('data_warehouse/wordcloud/machado_de_assis/memoriasBras.png'))
+    image.save(os.path.join(image_file))
     
