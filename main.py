ce8d0dc89a3a2d705c833ff73161b428d8a34f8e
Tiago Santos do Nascimento <tiagosn06@gmail.com>
2022-11-05 00:31:40 -0300

    create new function in main.py

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def exemplo_obra_machado_de_assis():
     csv_file_name = f"data_warehouse/{dataset_name}/{genero}/{text_name}.csv"
     txt_to_csv(file_path, csv_file_name, lang)
 
+def create_doc(text: str, metadata: dict):
+    """
+    INPUT:
+    text - str (the text data to be processed)
+    metadata - dict (name, source, url, genre, autor, date, language)
+    
+    Use Space models to process a string containing natural language data.
+    
+    OUTPUT:
+    database - save the features and the metadata in the database
+    http - return a json that can by an http response body.
+    """
 
 
 
